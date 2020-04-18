3c7e78a8339e531924f0554376f6487355989d16
alexesca <alexescamore@gmail.com>
2020-04-18 16:06:20 -0500

    Added sounds to  keys

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
+import AVFoundation
+
+
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
 
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.playSound(key: sender.currentTitle!)
+        }
+        
+    }
+    
+    func playSound(key: String) {
+        let url = Bundle.main.url(forResource: key, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
 }
 
